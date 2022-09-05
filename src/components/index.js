d95e765de58b886ecf0d10f6c6238f16743e0a5c
mohamdallam <mohamedahmedallam1@gmail.com>
2022-09-06 00:35:40 +0200

    home-page-hero

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,21 @@
 import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
 import Footer from "./Footer";
-import Cartbuttons from "./CartButtons";
+import Sidebar from "./Sidebar";
 import PageHero from "./PageHero";
+import Cartbuttons from "./CartButtons";
+import FeaturedProducts from "./FeaturedProducts";
+import Hero from "./Hero";
+import Services from "./Services";
+import Contact from "./Contact";
 
-export { Footer, Navbar, Sidebar, Cartbuttons, PageHero };
+export {
+  Footer,
+  Navbar,
+  Sidebar,
+  Cartbuttons,
+  PageHero,
+  FeaturedProducts,
+  Hero,
+  Services,
+  Contact,
+};
