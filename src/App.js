e4df9934b184a64b911fff0d7db288ebdb1df1be
mohamdallam <mohamedahmedallam1@gmail.com>
2022-09-05 21:27:23 +0200

    react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+} from "./pages";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Home />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/about">
+          <About />
+        </Route>
+
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+
+        <Route exact path="/products">
+          <Products />
+        </Route>
+
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
