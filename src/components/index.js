3173803f7103fd74fa2563d3f7614f9586b1f032
mohamdallam <mohamedahmedallam1@gmail.com>
2022-09-06 00:02:18 +0200

    about-checkout-page

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 import Cartbuttons from "./CartButtons";
+import PageHero from "./PageHero";
 
-export { Footer, Navbar, Sidebar, Cartbuttons };
+export { Footer, Navbar, Sidebar, Cartbuttons, PageHero };
